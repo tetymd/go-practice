47a317ec7e9f786180259b9fc0d9add899310bb8
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2018-09-15 18:02:45 +0900

    termboxを追加

--- examples/pty/main.go
+++ examples/pty/main.go
@@ -3,7 +3,9 @@ package main
 import (
     "fmt"
     "os"
+    "time"
     "github.com/kr/pty"
+    "github.com/nsf/termbox-go"
 )
 
 func main() {
@@ -16,6 +18,11 @@ func main() {
     defer t.Close()
     fmt.Println(p, t)
 
+    buf := make([]byte, 1024)
+    p.Write([]byte("\x1b[2J"))
+    n, err := p.Read(buf)
+    fmt.Println(string(buf[:n]))
+
     row, col, err := pty.Getsize(t)
     if err != nil {
         fmt.Println(err)
@@ -25,7 +32,7 @@ func main() {
 
     ws := &pty.Winsize{uint16(3), uint16(3), uint16(3), uint16(3)}
 
-    pty.Setsize(t, ws)
+    err = pty.Setsize(t, ws)
     if err != nil {
         fmt.Println(err)
         os.Exit(0)
@@ -37,5 +44,41 @@ func main() {
         os.Exit(0)
     }
     fmt.Println(row, col)
+
+    // fmt.Println("\x1b[2J")
+
+    err = termbox.Init()
+    if err != nil {
+        panic(err)
+    }
+    defer termbox.Close()
+
+    termbox.SetCursor(0, 0)
+
+    for {
+        switch ev := termbox.PollEvent(); ev.Type {
+            case termbox.EventKey:
+                switch ev.Key {
+                    case termbox.KeyEsc:
+                        fmt.Println("ESC")
+                        return
+                    case termbox.KeyEnter:
+                        fmt.Println("\n")
+                    case termbox.KeyArrowUp:
+                        fmt.Println("UP")
+                    case termbox.KeyArrowDown:
+                        fmt.Println("DOWN")
+                    default:
+                        fmt.Println("Other")
+                }
+            default:
+        }
+    }
+
+    fmt.Println("\x1b[7A")
+    time.Sleep(3 * time.Second)
+    fmt.Println("\x1b[3B")
+    time.Sleep(3 * time.Second)
+
 
 }
